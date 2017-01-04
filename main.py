3dd7e1cbaf6c622c5bf24282e25eb4b7002e2056
Billy Jeferson <skyfallbladez@gmail.com>
2017-01-04 17:35:54 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+print("Hello world\n")
+
+def main:
+	print("Welcome to my prototype project\n")
+	name = input("Please input your name: ")
+	age = input("Please input your age: ")
+	
+	print("Your name is %s. You are %d years old.")
